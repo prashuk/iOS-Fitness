9a93df09da635e47aca8a26688128b5c318b52d0
Prashuk <prashuk.ajmera@gmail.com>
2020-10-26 19:08:17 -0500

    Initial commit

---
+++ Fitness/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fitness
+//
+//  Created by Prashuk Ajmera on 10/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Prashuk Ajmera on 10/26/20.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
